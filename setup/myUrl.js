07a8f2a7149c4d1433c75b7d0f453fda63ab1715
kedar sedai <kedar.sedai05@gmail.com>
2020-04-15 13:56:58 +0545

    Update Packges

--- setup/myUrl.js
+++ setup/myUrl.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mydbUrl:'mongodb+srv://kedar:kedar123@cluster0-vntnn.mongodb.net/test?retryWrites=true&w=majority',
+    mydbUrl:'mongodb+srv://kedar:<password>@cluster0-vntnn.mongodb.net/test?retryWrites=true&w=majority',
     secret:'secret'
 }
